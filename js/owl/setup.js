1fe60a35ad7413ee1494fa4acec5d074f17381cf
raonicerqueira <raoni_henrique@live.com>
2021-06-15 09:21:37 -0300

    Projeto BootCamp DIO

---
+++ js/owl/setup.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        },
+        1500:{
+            items:8
+        },
+        2500:{
+            items:10
+        }
+
+        }
+    }
+)
